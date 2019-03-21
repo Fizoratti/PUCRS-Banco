f30017c6d4aee54b8afad7683f621e2a849ce894
matheusInhaia <matheus.inhaia2014@gmail.com>
2019-03-21 15:37:15 -0300

    testes do cadastro não funcionam

--- src/teste/CadastroContasTeste.java
+++ src/teste/CadastroContasTeste.java
@@ -3,14 +3,13 @@ package teste;
 
 import br.pucrs.ep.es.CadastroContas;
 import br.pucrs.ep.es.Conta;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CadastroContasTeste {
 
@@ -28,12 +27,25 @@ public class CadastroContasTeste {
 
     @Test
     public void removerContaTest(){
-
+        //não está funcionando
+        cadastro.removerConta("Matheus");
+        ArrayList<Conta> aux = new ArrayList<Conta>(){{
+            cadastro.inserirConta(new Conta("Bianca",25));
+            cadastro.inserirConta(new Conta("Vitor",63));
+            cadastro.inserirConta(new Conta("Maria",58));
+        }};
+        assertIterableEquals(aux,cadastro.getContas());
     }
 
     @Test
     public void pesquisarContaTest(){
-
+        //não está funcionando
+        cadastro.removerConta("Matheus");
+        ArrayList<Conta> aux = new ArrayList<Conta>();
+        aux.add(new Conta("Bianca",25));
+        aux.add(new Conta("Vitor",63));
+        aux.add(new Conta("Maria",58));
+        assertIterableEquals(aux,cadastro.getContas());
     }
 
 
